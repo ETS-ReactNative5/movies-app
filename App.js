66fee15e5c44ca8a92a597222665e4126c512627
Matti Ullah <mullah@jobcase.com>
2022-01-31 10:51:19 +0500

    Font Changed

--- App.js
+++ App.js
@@ -8,6 +8,7 @@
 import { Provider } from 'react-redux';
 
 import React from 'react';
+import {  Platform } from 'react-native';
 import { Navigation } from '../movies-app/src/navigation';
 import {
   SafeAreaView,
@@ -19,11 +20,23 @@ import {
 
 import store from './src/redux/store';
 
+import {
+  setCustomText,
+} from 'react-native-global-props';
+
+// Setting default styles for all Text components.
+const customTextProps = {
+  style: {
+    fontFamily: Platform.OS === 'ios' ? 'baskerville' : 'notoserif',
+  }
+};
+
+
+setCustomText(customTextProps);
 
 const App = () => {
   const isDarkMode = useColorScheme() === 'dark';
-
-
+ 
   return (
     <>
       <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
